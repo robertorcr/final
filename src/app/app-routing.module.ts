17cef0ccee0b7a6a2316f7648cbf4d4fcf035ccc
Gerardo Arroyo <asterisco.veinte@gmail.com>
2021-04-19 11:22:10 -0500

    anadir componente Gerardo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GerardoComponent } from './gerardo/gerardo.component';
 import { HomeComponent } from './home/home.component';
 import { RobertoPageComponent } from './roberto-page/roberto-page.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent}, 
   { path: 'home', component: HomeComponent },
   { path: 'roberto', component: RobertoPageComponent },
+  { path: 'Gerardo', component: GerardoComponent },
 ];
 
 @NgModule({
