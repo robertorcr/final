a666b93332d5ee44a22361dc34b7ca5435ffd76a
Roberto Gomez <tigrergz72mcm@hotmail.com>
2021-04-19 11:22:36 -0500

    añadi router zaratebis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RobertoPageComponent } from './roberto-page/roberto-page.component';
+import { ZaratebisComponent } from './zaratebis/zaratebis.component';
 
 const routes: Routes = [
+  { path: '', component:ZaratebisComponent },
   { path: 'home', component: HomeComponent },
   { path: 'roberto', component: RobertoPageComponent },
+  { path: 'zaratebis', component: ZaratebisComponent },
 ];
 
 @NgModule({
