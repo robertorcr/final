b1c3c73ecc90af62eb5783884009e7d59a258c56
Roberto Colorado <roberto.colorado@decsef.com>
2021-04-19 11:22:00 -0500

    añadí router vacío

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { RobertoPageComponent } from './roberto-page/roberto-page.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'roberto', component: RobertoPageComponent },
 ];
