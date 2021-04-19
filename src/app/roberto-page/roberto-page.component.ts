eee9fb5d38a4b6ca34dc801fcf011df33564d993
Roberto Colorado <roberto.colorado@decsef.com>
2021-04-19 14:01:15 -0500

    lectura archivo json

--- src/app/roberto-page/roberto-page.component.ts
+++ src/app/roberto-page/roberto-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import bicis from '../../assets/free-bike-status-1.json';
 
 @Component({
   selector: 'app-roberto-page',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roberto-page.component.css']
 })
 export class RobertoPageComponent implements OnInit {
-
-  constructor() { }
+ mybicis:any;
+  constructor() {
+  }
 
   ngOnInit(): void {
+
   }
+
+  leejson(){
+    console.log(bicis);
+  }
+
 
 }
