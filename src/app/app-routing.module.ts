fad95fe79c8dffe06db6f494fa778b3f28ab8cb7
diego <roberto.colorado@decsef.com>
2021-04-19 11:25:53 -0500

    cambios diego

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiegoComponent } from './diego/diego.component';
 import { HomeComponent } from './home/home.component';
 import { RobertoPageComponent } from './roberto-page/roberto-page.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'roberto', component: RobertoPageComponent },
+  { path: 'diego', component: DiegoComponent},
 ];
 
 @NgModule({
