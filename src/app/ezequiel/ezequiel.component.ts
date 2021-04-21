91a8a178b8192974a8009f87b8c68c01de3abb76
ezequiel <ezequielt1@hotmail.com>
2021-04-21 12:13:07 -0500

    cambios apariencia

--- src/app/ezequiel/ezequiel.component.ts
+++ src/app/ezequiel/ezequiel.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import bicis from '../../assets/free-bike-status-1.json';
 @Component({
   selector: 'app-ezequiel',
   templateUrl: './ezequiel.component.html',
   styleUrls: ['./ezequiel.component.css']
 })
 export class EzequielComponent implements OnInit {
-
-  constructor() { }
+mybicis: Array<any>=[];
+  
+constructor() { }
 
   ngOnInit(): void {
+  this.leejson();
   }
-
+leejson() {
+  this.mybicis=bicis.data.bikes;
+  console.log(this.mybicis);
+  console.log(bicis);
+}
 }
